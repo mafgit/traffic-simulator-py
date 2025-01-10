aed588e8a969a4f982062ed05f63a08866d7d5f6
Abdullah <mafishere@gmail.com>
2025-01-10 16:29:55 +0500

    added traffic light switching

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ ORANGE = (255, 165, 0)
 PINK = (255, 20, 147)
 PURPLE = (128, 0, 128)
 YELLOW = (255, 255, 0)
+CYAN = (0, 125, 255)
 
 # font
 font = pygame.font.Font(None, 15)
@@ -43,6 +44,11 @@ vehicles = [
     # Vehicle('LI', 'LN', 0.01, PINK, graph),
 ]
 
+traffic_signals = ['ITL', 'ITR', 'IBR', 'IBL']
+traffic_i = 0
+traffic_t = 0
+traffic_speed = 0.005
+
 # game loop
 clock = pygame.time.Clock()
 run = True
@@ -60,8 +66,14 @@ while run:
     
     # drawing vertices
     for v, vertex in graph.vertices.items():
-        # pygame.draw.rect(screen, BLUE if vertex['type']=='point' else RED, (vertex['pos'][0]-5, vertex['pos'][1]-5, 10, 10))
-        pygame.draw.circle(screen, BLUE if vertex['type']=='point' else RED, (vertex['pos'][0], vertex['pos'][1]), 3)
+        color = CYAN
+        if vertex['type'] == 'intersection':
+            if traffic_signals[traffic_i] == v:
+                color = GREEN
+            else:
+                color = RED
+
+        pygame.draw.circle(screen, color, (vertex['pos'][0], vertex['pos'][1]), 3)
         text_surface = font.render(v, True, WHITE)
         screen.blit(text_surface, (vertex['pos'][0]+2, vertex['pos'][1] + 2))
 
@@ -81,7 +93,14 @@ while run:
             
         pygame.draw.rect(screen, vehicle.color, (vehicle.x-5, vehicle.y-5, 10, 10))
         
-        
+    # updating traffic light
+    traffic_t += traffic_speed
+    if traffic_t >= 1:
+        traffic_t = 0
+        traffic_i += 1
+        if traffic_i > 3:
+            traffic_i = 0
+
     pygame.display.update()
     clock.tick(60)
 
